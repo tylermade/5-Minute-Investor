fa89fba7faf6032ca6fb97c65b9300fbc5cfcd41
Ki Eok Ryu <tryu@Kis-MacBook-Pro.local>
2018-11-12 23:27:25 +0800

    made basic API tests

--- 5mininvestor/core.py
+++ 5mininvestor/core.py
@@ -6,10 +6,17 @@ The Core handles API related functions
 '''
 
 import configparser
+import requests
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 API_KEY = config['DEFAULT']['API_KEY']
 
-print(API_KEY)
+FUNCTION = "TIME_SERIES_INTRADAY"
+SYMBOL = "MSFT"
+INTERVAL = "1min"
+API_URL = "https://www.alphavantage.co/query?outputsize=compact&function={}&symbol={}&interval={}&apikey={}"
+REQUEST = API_URL.format(FUNCTION, SYMBOL, INTERVAL, API_KEY)
+response = requests.get(REQUEST)
+print(response.text)
